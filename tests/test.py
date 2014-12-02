d657abd56828bde6a7f17e937db949b1e95ecd44
marcwebbie <marcwebbie@gmail.com>
2014-12-01 22:37:52 -0300

    Adding new class NotaFiscalEletronica

--- tests/test.py
+++ tests/test.py
@@ -3,9 +3,12 @@ import sys
 import unittest
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-import nfe
+from nfe import NotaFiscalEletronica
 
-class NfeTests(unittest.TestCase):
+class NotaFiscalEletronicaTests(unittest.TestCase):
     def test_nfe_instantiation(self):
-        new_nfe = nfe.nf_e()
-        self.assertIsNotNone(new_nfe)
+        nota_fiscal_eletronica = NotaFiscalEletronica()
+        self.assertIsNotNone(nota_fiscal_eletronica)
+
+if __name__ == "__main__":
+    unittest.main()
